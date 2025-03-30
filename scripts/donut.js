62c6e33ff878b852a589418bedbfbe9a1043fb19
mellowboyXD <chamerallyishfaq@gmail.com>
2025-03-30 09:26:11 +0400

    add datalabels and seperate variable that controls chart type

--- scripts/donut.js
+++ scripts/donut.js
@@ -1,10 +1,13 @@
 const ctx = document.getElementById("myDonut").getContext("2d");
+const chartType = "pie";
+
+Chart.register(ChartDataLabels);
 
 const data = {
   labels: ["Rent", "Food", "Transport", "Shopping", "Others"],
   datasets: [
     {
-      label: "# of Votes",
+      label: "$",
       data: [12, 19, 3, 5, 2],
       backgroundColor: ["red", "green", "orange", "purple", "blue"],
       borderWidth: 1,
@@ -19,13 +22,33 @@ const options = {
       display: true,
       position: "bottom",
     },
+    tooltip: {
+      enabled: true, 
+    },
+    datalabels: {
+      formatter: (value, ctx) => {
+        let sum = 0;
+        let dataArr = ctx.chart.data.datasets[0].data;
+        dataArr.map(data => {
+          sum += data;
+        });
+        let percentage = (value * 100 / sum).toFixed(1) + "%";
+        return percentage;
+      },
+      color: "#2e2e2e",
+      font: {
+        size: 13,
+        weight: "bold"
+      },
+    },
   },
 };
 
 const config = {
-  type: "doughnut",
+  type: chartType,
   data: data,
   options: options,
+  plugins: [ChartDataLabels],
 };
 
 new Chart(ctx, config);
