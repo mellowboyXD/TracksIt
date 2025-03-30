a4fcebb254d4907a8e157ed330a4cfe8362f1e0c
mellowboyXD <chamerallyishfaq@gmail.com>
2025-03-30 12:14:21 +0400

    add prefilled contents to edit modal

--- scripts/handleEdit.js
+++ scripts/handleEdit.js
@@ -1,5 +1,21 @@
 const editFormEl = document.getElementById('editForm');
+const tableRows = document.querySelectorAll('#expenses-table tbody tr');
+
+const descriptionEditEl = document.getElementById('descriptionEdit');
+const categoryEditEl = document.getElementById('categoryEdit');
+const dateEditEl = document.getElementById('dateEdit');
+const amountEditEl = document.getElementById('amountEdit');
 
 editFormEl.onsubmit = function (e) {
     e.preventDefault();
 }
+
+tableRows.forEach(row => {
+    row.addEventListener("click", (e) => {
+        const rowData = Array.from(row.cells).map(cell => cell.textContent);
+        descriptionEditEl.value = rowData[1];
+        categoryEditEl.value = rowData[2];
+        dateEditEl.value = rowData[3];
+        amountEditEl.value = rowData[4];
+    })
+})
