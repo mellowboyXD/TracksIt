2cf59d4c90ddb98b2684b1e25de4f26724584803
mellowboyXD <chamerallyishfaq@gmail.com>
2025-03-30 12:23:20 +0400

    Add hidden id to identify rows of data

--- scripts/handleEdit.js
+++ scripts/handleEdit.js
@@ -6,16 +6,29 @@ const categoryEditEl = document.getElementById('categoryEdit');
 const dateEditEl = document.getElementById('dateEdit');
 const amountEditEl = document.getElementById('amountEdit');
 
+const deleteButton = document.getElementById('deleteEntry');
+const saveEditButton = document.getElementById('saveEdit');
+
 editFormEl.onsubmit = function (e) {
     e.preventDefault();
 }
 
 tableRows.forEach(row => {
-    row.addEventListener("click", (e) => {
+    row.addEventListener('click', () => {
         const rowData = Array.from(row.cells).map(cell => cell.textContent);
-        descriptionEditEl.value = rowData[1];
-        categoryEditEl.value = rowData[2];
-        dateEditEl.value = rowData[3];
-        amountEditEl.value = rowData[4];
+        const id = rowData[1];
+        descriptionEditEl.value = rowData[2];
+        categoryEditEl.value = rowData[3];
+        dateEditEl.value = rowData[4];
+        amountEditEl.value = rowData[5];
+        console.log(id);
     })
+})
+
+deleteButton.addEventListener('click', () => {
+    // Delete from database
+})
+
+saveEditButton.addEventListener('click', () => {
+    // Update database
 })
