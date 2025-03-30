b536bd8edc6b45a4eff590e1c210d1c155a2eb2f
mellowboyXD <chamerallyishfaq@gmail.com>
2025-03-30 10:56:21 +0400

    add data validation on form submit

--- scripts/handleForm.js
+++ scripts/handleForm.js
@@ -1,11 +1,52 @@
-const addForm = document.getElementById('addForm');
+const addFormEl = document.getElementById('addForm');
+const descriptionEl = document.getElementById('description');
+const categoryEl = document.getElementById('category');
+const dateFieldEl = document.getElementById('date');
+const amountEl = document.getElementById('amount');
 
-addForm.onsubmit = function(e) {
+
+addFormEl.onsubmit = function(e) {
     e.preventDefault();
-    const description = document.getElementById('description').value;
-    const category = document.getElementById('category').value;
-    const dateField = document.getElementById('date').value;
-    const amount = document.getElementById('amount').value;
+    const description = descriptionEl.value;
+    const category = categoryEl.value;
+    const dateField = dateFieldEl.value;
+    const amount = amountEl.value;
     
-    console.log(dateField);
+    // Data Validation
+    if (!validateFormData(description, category, dateField, amount)) {
+        alert("All fields must be filled. Input correct data")
+    } else {
+        clearForm();
+    }
+}
+
+/**
+ * Validates the form input fields
+ * returns true is all valid else false
+ * 
+ * @param {String} desc - description
+ * @param {String} cat - category
+ * @param {String} dateField - date
+ * @param {String} amount
+ * @returns {Boolean} Either true or false
+ */
+function validateFormData(desc, cat, dateField, amount) {
+    amount = parseFloat(amount);
+    if (desc.length === 0 || dateField.length === 0 || cat === "category") {
+        return false;
+    } else if (amount.length === 0 || isNaN(amount)) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Clears the form input values
+ */
+function clearForm() {
+    descriptionEl.value = "";
+    amountEl.value = "";
+    categoryEl.value = "category";
+    dateFieldEl.value = "";
 }
