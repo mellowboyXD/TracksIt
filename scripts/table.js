19ee46aad7fa722bbeced049bf38b244a6238252
mellowboyXD <chamerallyishfaq@gmail.com>
2025-04-04 18:54:10 +0400

    seperate init functions for different pages

---
+++ scripts/table.js
@@ -0,0 +1,38 @@
+import { getAllFromDB } from "./db.js";
+
+export async function refreshTable() {
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = "";
+  try {
+    const res = await getAllFromDB();
+    let count = 0;
+    let sum = 0;
+    res.forEach((row) => {
+      // display
+      sum += parseFloat(row.amount);
+      const data = `
+                <tr data-bs-toggle="modal" data-bs-target="#editModal" class="row-data">
+                    <th scope="row">${++count}</th>
+                    <td class="hidden-cell">${row.id}</td>
+                    <td>${row.description}</td>
+                    <td>${row.category}</td>
+                    <td>${row.date}</td>
+                    <td>${row.amount}</td>
+                </tr>
+            `;
+      tableBody.innerHTML += data;
+    });
+
+    const sumEl = document.getElementById("sum-total");
+    sumEl.innerText = "$" + sum;
+  } catch (err) {
+    console.error("Error getting data. ", err);
+    return false;
+  }
+}
+
+if (window.location.href.endsWith("table.html")) {
+  window.addEventListener("DOMContentLoaded", async () => {
+    await refreshTable();
+  });
+}
