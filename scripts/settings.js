dda3833bb0705b772bbe2580a17fb6baf695b205
mellowboyXD <chamerallyishfaq@gmail.com>
2025-04-09 21:38:23 +0400

    add localStorage entry BUDGET_ENTRY

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,1 +1,29 @@
-const sliderEl = document.getElementById('budgetSlider');
+const sliderEl = document.getElementById("budget-slider");
+const budgetAmountEl = document.getElementById("budget-amount");
+const saveBtn = document.getElementById("save-settings");
+
+const DEFAULT_BUDGET = "250";
+const BUDGET_ENTRY = "budget";
+let budget = getBudget();
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.location.href.includes("settings")) {
+    budgetAmountEl.innerText = `$${budget}`;
+    sliderEl.value = budget;
+
+    saveBtn.addEventListener("click", () => {
+        window.localStorage.setItem(BUDGET_ENTRY, sliderEl.value);
+        window.location.reload();
+    })
+  }
+});
+
+export function getBudget() {
+    let budgetAmount = window.localStorage.getItem(BUDGET_ENTRY);
+    if(!budgetAmount) {
+        window.localStorage.setItem(BUDGET_ENTRY, DEFAULT_BUDGET);
+        return window.localStorage.getItem(BUDGET_ENTRY);;
+    } else {
+        return budgetAmount;
+    }
+}
