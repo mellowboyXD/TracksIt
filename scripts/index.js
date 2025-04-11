34cdbd4623e882b8a1ab40a3a08565f48bd95367
mellowboyXD <chamerallyishfaq@gmail.com>
2025-04-11 14:52:26 +0400

    add offline capability

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,8 @@
-import { chart } from "./dashboardChart.js";
-import { getAllFromDB, getRangeFromDB } from "./db.js";
+import { getRangeFromDB } from "./db.js";
 import { getBudget } from "./settings.js";
 
 export async function fetchData() {
     try {
-        // const data = await getAllFromDB();
         const data = await getRangeFromDB();
         const budget = getBudget();
         const budgetEl = document.getElementById('budget');    
@@ -20,6 +18,54 @@ export async function refreshChart() {
     window.location.reload();
 }
 
+async function checkSwRegistration() {
+    let swLocation = "sw.js";
+    if(window.location.href.includes("settings") || window.location.href.includes("table")) {
+        swLocation = "../sw.js"
+    }
+    if("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register(swLocation);
+            console.log("Service worker registered");   
+        } catch(err) {
+            console.error("Error registering service worker: ", err);
+        }
+    } else {
+        console.log("Service Worker API not available");
+    }
+}
+
+async function unregisterSw() {
+    if("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.getRegistration();
+            
+            if(registration) {
+                const result = registration.unregister();
+                if(result) {
+                    console.log("Service worker unregistered");
+                } else {
+                    console.error("Service worker could not be unregistered");
+                }
+            } else {
+                console.error("Could not get service worker registration: ");
+            }
+        } catch(err) {
+            console.error("Failed to unregister service worker: ", err);
+        }
+    } else {
+        console.log("Service Worker API not available");
+    }
+}
+
+function updateOnlineStatus() {
+    console.log(navigator.onLine ? "online" : "offline");
+    
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
+    await checkSwRegistration();
+    // await unregisterSw();
+    updateOnlineStatus();
     await fetchData();
 });
